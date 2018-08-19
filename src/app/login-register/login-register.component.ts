cf3c80751b798a92b097d28ef2b2fba07d77127d
BK610 <bailey.orion.kane@gmail.com>
2018-08-19 14:22:52 -0400

    copied register code

--- src/app/login-register/login-register.component.ts
+++ src/app/login-register/login-register.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {UserService} from '../services/user.service';
 
 @Component({
-  selector: 'app-login-register',
-  templateUrl: './login-register.component.html',
-  styleUrls: ['./login-register.component.css']
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
 })
 export class LoginRegisterComponent implements OnInit {
+  username: '';
+  password: '';
+  password2: '';
+  constructor(private router: Router,
+              private userService: UserService) {
+  }
 
-  constructor() { }
+  register = (user) => {
+    if (user.password === user.password2) {
+      const newUser = {
+        username: user.username,
+        password: user.password,
+        firstName: String,
+        lastName: String,
+        email: String,
+        phone: String,
+        role: 'student',
+        sections: []
+      };
+      if (user.username === 'admin' && user.password === 'admin') {
+        newUser.role = 'admin';
+      }
+      this.userService.register(newUser)
+        .then(status => {
+          if (status === 200) {
+            return this.router.navigate(['/profile']);
+          } else {
+            alert('Register failed. Username may already exist.');
+          }
+        });
+    } else {
+      alert('Passwords must match.');
+    }
+  }
 
   ngOnInit() {
   }
-
 }
