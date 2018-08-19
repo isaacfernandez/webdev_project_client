7e3af99cca2433459543c1958928ce841173909e
root <root@localdomain>
2018-08-19 14:43:28 -0400

    Simple navbar and navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,10 @@ import { UserComponent } from './user/user.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
-import { LoginRegisterComponent } from './login-register/login-register.component';
+//import { LoginRegisterComponent } from './login-register/login-register.component';
+import { ProfileComponent } from './profile/profile.component';
+import {routing} from "./app.routing";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -19,10 +22,12 @@ import { LoginRegisterComponent } from './login-register/login-register.componen
     UserListComponent,
     NavBarComponent,
     HomeComponent,
-    LoginRegisterComponent
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/profile/profile.component.spec.ts
+++ src/app/profile/profile.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoginRegisterComponent } from './login-register.component';
+import { ProfileComponent } from './profile.component';
 
-describe('LoginRegisterComponent', () => {
-  let component: LoginRegisterComponent;
-  let fixture: ComponentFixture<LoginRegisterComponent>;
+describe('ProfileComponent', () => {
+  let component: ProfileComponent;
+  let fixture: ComponentFixture<ProfileComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginRegisterComponent ]
+      declarations: [ ProfileComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LoginRegisterComponent);
+    fixture = TestBed.createComponent(ProfileComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
