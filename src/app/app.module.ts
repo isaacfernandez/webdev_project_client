1440ff2db0c99b7f2a9a351b6d842b2d2f9958bf
BK610 <bailey.orion.kane@gmail.com>
2018-08-19 14:11:48 -0400

    Created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FeedComponent } from './feed/feed.component';
+import { FeedListComponent } from './feed-list/feed-list.component';
+import { UserComponent } from './user/user.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedComponent,
+    FeedListComponent,
+    UserComponent,
+    UserListComponent,
+    NavBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
