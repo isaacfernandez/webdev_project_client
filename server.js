04e3fe9c5d1d8b3c1dac33e6a03ac553430178a2
BK610 <bailey.orion.kane@gmail.com>
2018-08-19 23:04:35 -0400

    Created server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+app.use(express.static('./dist/webdev-project-client'));
+
+// app.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin',
+//     'http://localhost:4200');
+//   res.header('Access-Control-Allow-Credentials',
+//     'true');
+//   res.header('Access-Control-Allow-Methods',
+//     'GET, POST, PUT, DELETE, OPTIONS');
+//   res.header('Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join('./dist/webdev-project-client/index.html'));
+});
+app.listen(process.env.PORT || 4200);
