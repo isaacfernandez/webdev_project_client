e6df27887b722d70749574888ed9cdd9c7ee21e5
root <root@localdomain>
2018-08-19 20:29:08 -0400

    nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-
+import {ActivatedRoute} from "@angular/router";
 import { AppComponent } from './app.component';
 import { FeedComponent } from './feed/feed.component';
 import { FeedListComponent } from './feed-list/feed-list.component';
